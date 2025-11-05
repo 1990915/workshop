15c038393f09a3ad1acda78db90c2d3d1e08fbe3
h_dinari <h_dinari@modernisc.com>
2025-11-05 17:06:36 +0330

    #initialMsg

--- src/main/java/com/misc/Main.java
+++ src/main/java/com/misc/Main.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
+        System.out.println("This is a text!");
         while (true) {
             File file = new File("text.txt");
             FileWriter fileWriter = new FileWriter(file);
